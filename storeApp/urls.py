127a773d2c374b88115f161c99923111361f48fa
ABDULRAHMAN <business.zinon@gmail.com>
2021-03-22 13:31:05 +0200

    add to cart

--- storeApp/urls.py
+++ storeApp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import *
+from . import views
 
 urlpatterns = [
     path('', Stores.as_view(), name='stores'),
+    path('cart', views.cart, name='cart'),
+    path('checkout', views.checkout, name='checkout'),
 
 ]
--- storeApp/views.py
+++ storeApp/views.py
@@ -9,4 +9,9 @@ class Stores(ListView):
     model = Product
     template_name = 'store/store.html'
 
+def cart(request):
+    return render(request, 'store/cart.html')
 
+
+def checkout(request):
+    return render(request, 'store/checkout.html')
