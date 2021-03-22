78b3eb13836f92517a9a154f84eb6795a71a488b
ABDULRAHMAN <business.zinon@gmail.com>
2021-03-22 14:09:44 +0200

    storeApp

--- storeApp/views.py
+++ storeApp/views.py
@@ -1,17 +1,117 @@
-from django.views.generic import ListView
 from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic import ListView
 
 from dashboardApp.models import *
-# Create your views here.
+from .utils import cookieCart, cartData, guestOrder
 
+from django.http import JsonResponse
+import json
+import datetime
+
+# Create your views here.
 
 class Stores(ListView):
     model = Product
     template_name = 'store/store.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        data = cartData(self.request)
+        context['cartItems'] = data['cartItems']
+        return context
+
+
+
 def cart(request):
-    return render(request, 'store/cart.html')
+    data = cartData(request)
+    items = data['items']
+    order = data['order']
+    cartItems = data['cartItems']
+
+    context = {
+        'items': items,
+        'order': order,
+        'cartItems': cartItems,
+    }
+    return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-    return render(request, 'store/checkout.html')
+    data = cartData(request)
+    items = data['items']
+    order = data['order']
+    cartItems = data['cartItems']
+
+    context = {
+            'items': items,
+            'order': order,
+            'cartItems': cartItems
+        }
+    return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
+
+
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    else:
+        customer, order = guestOrder(request, data)
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+        )
+
+    return JsonResponse('Payment submitted..', safe=False)
+
+
+
+
+
+
+
+
